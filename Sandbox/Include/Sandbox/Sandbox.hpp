6feb784017fa0212f0c23ea72bfe50032925d688
Vyom Fadia <vyom.fadia.work@gmail.com>
2020-01-11 17:03:56 +0000

    Just put something normal in here real quick.

--- Sandbox/Include/Sandbox/Sandbox.hpp
+++ Sandbox/Include/Sandbox/Sandbox.hpp
@@ -1,206 +1,44 @@
-#include <Quartz2/Quartz.hpp>
-#include <UI.hpp>
+// Copyright 2019 Genten Studios
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-#include <imgui.h>
+#pragma once
 
-#include <iostream>
-#include <sstream>
-#include <memory>
-#include <string>
-
-using namespace q2;
-
-
-static void rawEcho(const std::string &input, std::ostringstream &cout)
+namespace sandbox
 {
-	// easter egg commission for the tobster.
-	if (input.compare("buh-buh-bum-bah-bum") == 0) cout << "I'll be back\n";
-
-	cout << input << "\n";
-}
-
-static ui::ChatWindow chat("Chat Window", 5,
-	// @FutureRuby I know this looks weird but it works.
-	// constant string compile time concatenation is wierd.
-	"Welcome to the Darklight Terminal!\n"
-	"Type something and hit enter to run a command!\n"
-);
-
-static ImGui::BasicTerminal term("Test Terminal", 5);
-
-class PhoenixGame : public Game
-{
-private:
-	std::unique_ptr<ChunkRenderer> m_chunkRenderer;
-	std::unique_ptr<Camera> m_camera;
-
-public:
-	PhoenixGame()
-		: Game(1280, 720, "Project Phoenix!")
+	class Sandbox
 	{
+	public:
+		Sandbox() = default;
+		~Sandbox() = default;
 
-		chat.registerCallback(&rawEcho);
-		term.registerCallback(&rawEcho);
-	}
+		int helloSandbox() const { return m_sandboxNumber; }
 
-protected:
-	virtual bool onEvent(SDL_Event e)
-	{
-		if (e.type == SDL_KEYUP)
-		{
-			if (e.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
-			{
-				m_camera->enable(!m_camera->isEnabled());
-				SDL_ShowCursor(!m_camera->isEnabled());
-				return true;
-			}
-			// Uncomment to have chat popup every time enter is pressed.
-			// Could probably use integration with @SonosFuer's input system.
-			// if (e.key.keysym.scancode == SDL_SCANCODE_RETURN)
-			// NOTE:
-			//   doing this encurs the wrath of cthulu and you will be cursed to
-			//   oblivion. By which I mean you won't be able to press enter without
-			//   the chat window being focused on period, regardless of other ui
-			//   elements being overlayed. I tryed to make this a non-issue by
-			//   by leaving the ImGuiWindowFlags_NoFocusOnAppearing flag with the
-			//   class definition but it can be a pain when using a second terminal.
-			//
-			// {
-			// 	chat.focus();
-			// 	return true;
-			// }
-		}
-
-		return false;
-	}
-
-	virtual void onStart()
-	{
-		sol::state lua;
-		lua.open_libraries(sol::lib::base);
-		luaapi::loadAPI(lua);
-		bool loadedLua = modules::loadModules("save1", lua);
-		if (!loadedLua){
-			exitGame();
-		}
-
-		m_chunkRenderer = std::make_unique<ChunkRenderer>();
-		m_chunkRenderer->setup(getWindowWidth(), getWindowHeight());
-
-		if (!m_chunkRenderer->isReady())
-		{
-			std::fprintf(stderr, "Renderer setup failed: Status = %s\n",
-				enums::toString(m_chunkRenderer->status()));
-
-			exitGame();
-		}
-
-		m_camera = std::make_unique<Camera>();
-		SDL_ShowCursor(0);
-
-		std::shared_ptr<BlockTextureAtlas>atlas = std::make_shared<BlockTextureAtlas>(16, 16);
-		atlas->addTextureFile("Assets/grass_top.png");
-		atlas->addTextureFile("Assets/dirt.png");
-		atlas->addTextureFile("Assets/grass_side.png");
-		atlas->patch();
-
-		BlockRegistry* blocksRegistery = BlockRegistry::get();
-		blocksRegistery->setAtlas(atlas);
-
-		blocksRegistery->registerBlock({ "Air", "core:air", BLOCK_CATEGORY_AIR, {} });
-
-		BlockType* dirtBlockType = blocksRegistery->registerBlock({ "Dirt", "core:dirt", BLOCK_CATEGORY_SOLID, {} });
-		BlockType* grassBlockType = blocksRegistery->registerBlock({ "Grass", "core:grass", BLOCK_CATEGORY_SOLID, {} });
-
-		dirtBlockType->textures.setAll(atlas->getSpriteIDFromFilepath("Assets/dirt.png"));
-
-		grassBlockType->textures.top = atlas->getSpriteIDFromFilepath("Assets/grass_top.png");
-		grassBlockType->textures.bottom = atlas->getSpriteIDFromFilepath("Assets/dirt.png");
-		grassBlockType->textures.front = grassBlockType->textures.back
-			= grassBlockType->textures.left = grassBlockType->textures.right =
-				atlas->getSpriteIDFromFilepath("Assets/grass_side.png");
-
-		m_chunkRenderer->setTexture(atlas->getPatchedTextureData(), atlas->getPatchedTextureWidth(), atlas->getPatchedTextureHeight());
-
-		Chunk a;
-		m_chunkRenderer->addMesh(a.generateMesh());
-	}
-
-	virtual void onExit()
-	{
-		m_chunkRenderer->teardown();
-	}
-
-	void showDebugStats(int fps, float dt)
-	{
-		const float DISTANCE = 10.0f;
-		const int   corner = 1;
-		static bool p_open = true;
-
-		ImGuiIO& io = ImGui::GetIO();
-
-		ImVec2 window_pos =
-			ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE,
-			(corner & 2) ? io.DisplaySize.y - DISTANCE * 2.5f : DISTANCE * 2.5f);
-		ImVec2 window_pos_pivot =
-			ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
-		ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
-
-		ImGui::SetNextWindowBgAlpha(0.3f);
-
-		ImGui::Begin(
-			"Debug Overlay Hint", &p_open,
-			(corner != -1 ? ImGuiWindowFlags_NoMove : 0) |
-			ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
-			ImGuiWindowFlags_AlwaysAutoResize |
-			ImGuiWindowFlags_NoSavedSettings |
-			ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav
-		);
-
-		ImGui::Text("FPS %i\n", (int)fps);
-		ImGui::Text("Delta time: %f\n", dt);
-
-		ImGui::End();
-	}
-
-	virtual void onFrame(float dt)
-	{
-		static bool wireframe = false;
-
-		m_camera->tick(dt, getSDLWindow());
-
-		OpenGL32::clearScreen(0.2196f, 0.2196f, 0.2196f, 1.f);
-
-		showDebugStats(getFPS(), dt);
-
-		ImGui::Begin("Debug View");
-		if (ImGui::Checkbox("Wireframe", &wireframe))
-		{
-			if (wireframe)
-				OpenGL32::enableWireframe();
-			else
-				OpenGL32::disableWireframe();
-		}
-		ImGui::Text("OpenGL Error %i\n", glGetError());
-		ImGui::End();
-
-		// Vertical height adjust the chat window... happens in the UI
-		// segment definition because Main function management is hard enough
-		// as it is. Here's to the good time of whomever doesn't have to deal
-		// with 20 extra lines of clutter because of me :D
-		chat.draw();
-		term.draw();
-
-		ImGui::ShowStyleEditor();
-		m_chunkRenderer->render(m_camera.get());
-	}
-};
-
-#undef main
-int main(int argc, char** argv)
-{
-	Game* game = new PhoenixGame();
-	game->start();
-
-	return 0;
-}
+	private:
+		int m_sandboxNumber = 1;
+	};
+} // namespace sandbox
