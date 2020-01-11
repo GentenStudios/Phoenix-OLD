7bfc0ae88a76a2f62ab1089c08396f5f3c8455d2
Vyom Fadia <vyom.fadia.work@gmail.com>
2020-01-11 23:31:37 +0000

    Implement new windowing system.

--- Sandbox/Source/Main.cpp
+++ Sandbox/Source/Main.cpp
@@ -1,5 +1,7 @@
 #include <Quartz2/Events/Event.hpp>
+#include <Quartz2/Graphics/Window.hpp>
 #include <Quartz2/Quartz.hpp>
+
 #include <Sandbox/UI.hpp>
 
 #include <imgui.h>
@@ -13,10 +15,6 @@ using namespace q2;
 
 static void rawEcho(const std::string& input, std::ostringstream& cout)
 {
-	// easter egg commission for the tobster.
-	if (input.compare("buh-buh-bum-bah-bum") == 0)
-		cout << "I'll be back\n";
-
 	cout << input << "\n";
 }
 
@@ -27,201 +25,117 @@ static ui::ChatWindow chat(
     "Welcome to the Darklight Terminal!\n"
     "Type something and hit enter to run a command!\n");
 
-static ImGui::BasicTerminal term("Test Terminal", 5);
-
-class PhoenixGame : public Game
+class Phoenix : public events::IEventListener
 {
-private:
-	std::unique_ptr<ChunkRenderer> m_chunkRenderer;
-	std::unique_ptr<Camera>        m_camera;
-
 public:
-	PhoenixGame() : Game(1280, 720, "Project Phoenix!")
+	Phoenix()
 	{
-
-		chat.registerCallback(&rawEcho);
-		term.registerCallback(&rawEcho);
+		m_window = new gfx::Window("Phoenix Game!", 1280, 720);
+		m_window->registerEventListener(this);
 	}
 
-protected:
-	virtual bool onEvent(SDL_Event e)
+	~Phoenix() { delete m_window; }
+
+	void onEvent(const events::Event& e) override
 	{
-		if (e.type == SDL_KEYUP)
+		switch (e.type)
 		{
-			if (e.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+		case events::EventType::KEY_PRESSED:
+			switch (e.keyboard.key)
 			{
-				m_camera->enable(!m_camera->isEnabled());
-				SDL_ShowCursor(!m_camera->isEnabled());
-				return true;
+			case events::Keys::KEY_ESCAPE:
+				m_window->close();
+				break;
+
+			default:
+				break;
 			}
-			// Uncomment to have chat popup every time enter is pressed.
-			// Could probably use integration with @SonosFuer's input system.
-			// if (e.key.keysym.scancode == SDL_SCANCODE_RETURN)
-			// NOTE:
-			//   doing this encurs the wrath of cthulu and you will be cursed to
-			//   oblivion. By which I mean you won't be able to press enter
-			//   without the chat window being focused on period, regardless of
-			//   other ui elements being overlayed. I tryed to make this a
-			//   non-issue by by leaving the ImGuiWindowFlags_NoFocusOnAppearing
-			//   flag with the class definition but it can be a pain when using
-			//   a second terminal.
-			//
-			// {
-			// 	chat.focus();
-			// 	return true;
-			// }
-		}
+			break;
 
-		return false;
+		default:
+			break;
+		}
 	}
 
-	virtual void onStart()
+	void run()
 	{
-		events::Position pos = { 12, 12 };
-		events::Size     size   = {pos.x, pos.y};
-		events::Scroll   scroll = {static_cast<float>(size.width),
-                                 static_cast<float>(size.height)};
-		events::Keyboard key    = {events::Keys::KEY_0, events::Mods::MOD_CAPS};
-
-		if (scroll.x == 0)
+		while (m_window->isRunning())
 		{
-			std::cout
-			    << "fun times, i am literally trying to make codacy pass...";
-			std::cout << static_cast<int>(key.key) << static_cast<int>(key.mods);
+			m_window->startFrame();
+
+			ImGui::ShowDemoWindow();
+			
+			m_window->endFrame();
 		}
-
-		sol::state lua;
-		lua.open_libraries(sol::lib::base);
-		luaapi::loadAPI(lua);
-		bool loadedLua = modules::loadModules("save1", lua);
-		if (!loadedLua)
-		{
-			exitGame();
-		}
-
-		m_chunkRenderer = std::make_unique<ChunkRenderer>();
-		m_chunkRenderer->setup(getWindowWidth(), getWindowHeight());
-
-		if (!m_chunkRenderer->isReady())
-		{
-			std::fprintf(stderr, "Renderer setup failed: Status = %s\n",
-			             enums::toString(m_chunkRenderer->status()));
-
-			exitGame();
-		}
-
-		m_camera = std::make_unique<Camera>();
-		SDL_ShowCursor(0);
-
-		std::shared_ptr<BlockTextureAtlas> atlas =
-		    std::make_shared<BlockTextureAtlas>(16, 16);
-		atlas->addTextureFile("Assets/grass_top.png");
-		atlas->addTextureFile("Assets/dirt.png");
-		atlas->addTextureFile("Assets/grass_side.png");
-		atlas->patch();
-
-		BlockRegistry* blocksRegistery = BlockRegistry::get();
-		blocksRegistery->setAtlas(atlas);
-
-		blocksRegistery->registerBlock(
-		    {"Air", "core:air", BLOCK_CATEGORY_AIR, {}});
-
-		BlockType* dirtBlockType = blocksRegistery->registerBlock(
-		    {"Dirt", "core:dirt", BLOCK_CATEGORY_SOLID, {}});
-		BlockType* grassBlockType = blocksRegistery->registerBlock(
-		    {"Grass", "core:grass", BLOCK_CATEGORY_SOLID, {}});
-
-		dirtBlockType->textures.setAll(
-		    atlas->getSpriteIDFromFilepath("Assets/dirt.png"));
-
-		grassBlockType->textures.top =
-		    atlas->getSpriteIDFromFilepath("Assets/grass_top.png");
-		grassBlockType->textures.bottom =
-		    atlas->getSpriteIDFromFilepath("Assets/dirt.png");
-		grassBlockType->textures.front    = grassBlockType->textures.back =
-		    grassBlockType->textures.left = grassBlockType->textures.right =
-		        atlas->getSpriteIDFromFilepath("Assets/grass_side.png");
-
-		m_chunkRenderer->setTexture(atlas->getPatchedTextureData(),
-		                            atlas->getPatchedTextureWidth(),
-		                            atlas->getPatchedTextureHeight());
-
-		Chunk a;
-		m_chunkRenderer->addMesh(a.generateMesh());
 	}
 
-	virtual void onExit() { m_chunkRenderer->teardown(); }
-
-	void showDebugStats(int fps, float dt)
-	{
-		const float DISTANCE = 10.0f;
-		const int   corner   = 1;
-		static bool p_open   = true;
-
-		ImGuiIO& io = ImGui::GetIO();
-
-		ImVec2 window_pos =
-		    ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE,
-		           (corner & 2) ? io.DisplaySize.y - DISTANCE * 2.5f
-		                        : DISTANCE * 2.5f);
-		ImVec2 window_pos_pivot =
-		    ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
-		ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
-
-		ImGui::SetNextWindowBgAlpha(0.3f);
-
-		ImGui::Begin(
-		    "Debug Overlay Hint", &p_open,
-		    (corner != -1 ? ImGuiWindowFlags_NoMove : 0) |
-		        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
-		        ImGuiWindowFlags_AlwaysAutoResize |
-		        ImGuiWindowFlags_NoSavedSettings |
-		        ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav);
-
-		ImGui::Text("FPS %i\n", static_cast<int>(fps));
-		ImGui::Text("Delta time: %f\n", dt);
-
-		ImGui::End();
-	}
-
-	virtual void onFrame(float dt)
-	{
-		static bool wireframe = false;
-
-		m_camera->tick(dt, getSDLWindow());
-
-		OpenGL32::clearScreen(0.2196f, 0.2196f, 0.2196f, 1.f);
-
-		showDebugStats(getFPS(), dt);
-
-		ImGui::Begin("Debug View");
-		if (ImGui::Checkbox("Wireframe", &wireframe))
-		{
-			if (wireframe)
-				OpenGL32::enableWireframe();
-			else
-				OpenGL32::disableWireframe();
-		}
-		ImGui::Text("OpenGL Error %i\n", glGetError());
-		ImGui::End();
-
-		// Vertical height adjust the chat window... happens in the UI
-		// segment definition because Main function management is hard enough
-		// as it is. Here's to the good time of whomever doesn't have to deal
-		// with 20 extra lines of clutter because of me :D
-		chat.draw();
-		term.draw();
-
-		ImGui::ShowStyleEditor();
-		m_chunkRenderer->render(m_camera.get());
-	}
+private:
+	gfx::Window* m_window;
 };
 
 #undef main
 int main(int argc, char** argv)
 {
-	Game* game = new PhoenixGame();
-	game->start();
+	Phoenix* game = new Phoenix();
+	game->run();
 
 	return 0;
 }
+
+//		sol::state lua;
+// lua.open_libraries(sol::lib::base);
+// luaapi::loadAPI(lua);
+// bool loadedLua = modules::loadModules("save1", lua);
+// if (!loadedLua)
+//{
+//	exitGame();
+//}
+//
+// m_chunkRenderer = std::make_unique<ChunkRenderer>();
+// m_chunkRenderer->setup(getWindowWidth(), getWindowHeight());
+//
+// if (!m_chunkRenderer->isReady())
+//{
+//	std::fprintf(stderr, "Renderer setup failed: Status = %s\n",
+//	             enums::toString(m_chunkRenderer->status()));
+//
+//	exitGame();
+//}
+//
+// m_camera = std::make_unique<Camera>();
+// SDL_ShowCursor(0);
+//
+// std::shared_ptr<BlockTextureAtlas> atlas =
+//    std::make_shared<BlockTextureAtlas>(16, 16);
+// atlas->addTextureFile("Assets/grass_top.png");
+// atlas->addTextureFile("Assets/dirt.png");
+// atlas->addTextureFile("Assets/grass_side.png");
+// atlas->patch();
+//
+// BlockRegistry* blocksRegistery = BlockRegistry::get();
+// blocksRegistery->setAtlas(atlas);
+//
+// blocksRegistery->registerBlock({"Air", "core:air", BLOCK_CATEGORY_AIR, {}});
+//
+// BlockType* dirtBlockType = blocksRegistery->registerBlock(
+//    {"Dirt", "core:dirt", BLOCK_CATEGORY_SOLID, {}});
+// BlockType* grassBlockType = blocksRegistery->registerBlock(
+//    {"Grass", "core:grass", BLOCK_CATEGORY_SOLID, {}});
+//
+// dirtBlockType->textures.setAll(
+//    atlas->getSpriteIDFromFilepath("Assets/dirt.png"));
+//
+// grassBlockType->textures.top =
+//    atlas->getSpriteIDFromFilepath("Assets/grass_top.png");
+// grassBlockType->textures.bottom =
+//    atlas->getSpriteIDFromFilepath("Assets/dirt.png");
+// grassBlockType->textures.front    = grassBlockType->textures.back =
+//    grassBlockType->textures.left = grassBlockType->textures.right =
+//        atlas->getSpriteIDFromFilepath("Assets/grass_side.png");
+//
+// m_chunkRenderer->setTexture(atlas->getPatchedTextureData(),
+//                            atlas->getPatchedTextureWidth(),
+//                            atlas->getPatchedTextureHeight());
+//
+// Chunk a;
+// m_chunkRenderer->addMesh(a.generateMesh());
