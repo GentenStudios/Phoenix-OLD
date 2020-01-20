7353026d2f099e86039faf7a478bf82c4fa4d5f5
Austin <pachano.austin@gmail.com>
2020-01-19 17:51:05 -0800

    Add flattening functions to MathUtils and Vector 3, add de-flattening constructor to Vector 3

--- Phoenix/Include/Phoenix/Math/Vector3.hpp
+++ Phoenix/Include/Phoenix/Math/Vector3.hpp
@@ -110,6 +110,47 @@ namespace phx
 					return vec;
 				}
 
+				/**
+				 * @brief Create a Vector3 from a flattened cube number and cube side length
+				 * 
+				 * @tparam X Flattened cube number
+				 * @tparam Len Cube side length
+				 */
+				template <typename X, typename Len>
+				constexpr explicit Vector3(X val, Len len )
+				{
+					z = static_cast<T>((val / (len * len);
+					y = static_cast<T>((val - (z * len * len))/ len);
+					x = static_cast<T>((val - len * (y + (len * z))));
+				}
+				
+				/**
+				 * @brief Flattens a Vector3 representing a coordinate in a 
+				 * rectangle into a single value
+				 * 
+				 * @tparam T
+				 * @param lenX Length of side X in rectangle
+				 * @param lenY Length of side Y in rectangle
+				 * @return constexpr T  Flattened position
+				 */
+				template <typename T>
+				constexpr T flatten(T lenX, T lenY){
+					return x + lenX * (y + lenY * z);
+				}
+
+				/**
+				 * @brief Flattens a Vector3 representing a coordinate in a 
+				 * square into a single value
+				 * 
+				 * @tparam T
+				 * @param len Length of a side in the square
+				 * @return constexpr T Flattened position
+				 */
+				template <typename T>
+				constexpr T flatten(T len){
+					return x + len * (y + len * z);
+				}
+
 				void floor()
 				{
 					x = std::floor(x);
