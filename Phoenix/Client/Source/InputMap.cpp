febab58d97032c0492f21cfa5f899014cf827997
Austin <pachano.austin@gmail.com>
2020-04-11 09:07:47 -0700

    THe inputMap needs to stay in the client for now

--- Phoenix/Client/Source/InputMap.cpp
+++ Phoenix/Client/Source/InputMap.cpp
@@ -26,7 +26,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include <Common/InputMap.hpp>
+#include <Client/InputMap.hpp>
 
 #include <Common/ContentLoader.hpp>
 
